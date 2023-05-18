b9436d0811830febb7aeeaa46d7bc6953c2feb63
Ian Blake <ian@ianbl8.com>
2023-05-18 14:17:12 +0100

    Implement Leaflet.js map page

---
+++ src/lib/DonationMap.svelte
@@ -0,0 +1,19 @@
+<script>
+	import 'leaflet/dist/leaflet.css';
+	import { LeafletMap } from '../services/leaflet-map';
+	import { onMount } from 'svelte';
+
+	const mapConfig = {
+		location: { lat: 52.160858, lng: -7.15242 },
+		zoom: 8,
+		minZoom: 1
+	};
+
+	onMount(async () => {
+		const map = new LeafletMap('donation-map', mapConfig);
+		map.showZoomControl();
+		map.showLayerControl();
+	});
+</script>
+
+<div class="box" id="donation-map" style="height:75vh" />
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,12 +1,12 @@
 <script>
-  import TitleBar from './TitleBar.svelte';
+	import TitleBar from './TitleBar.svelte';
 </script>
 
 <div class="columns is-vcentered">
-  <div class="column is-two-thirds">
-      <TitleBar title={'Donation Services Inc.'} subtitle={'Sign up or Log in'} />
-  </div>
-  <div class="column">
-      <slot />
-  </div>
+	<div class="column is-half">
+		<TitleBar title={'Donation Services Inc.'} subtitle={'Sign up or Log in'} />
+	</div>
+	<div class="column is-half">
+		<slot />
+	</div>
 </div>
